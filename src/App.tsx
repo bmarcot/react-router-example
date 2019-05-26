74bce665b163e6b1f874b64f3aa297f2a39f9cc7
Benoit Marcot <juz4m@hotmail.com>
2019-05-26 18:40:57 +0200

    Remove useless Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Requirements from "./Requirements";
 
@@ -24,10 +24,8 @@ const App: React.FC = () => {
           </ul>
         </nav>
         <hr />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/requirements" component={Requirements} />
-        </Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/requirements" component={Requirements} />
       </div>
     </Router>
   );
